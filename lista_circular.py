2d2cb3f1bf3ec5faee1ed83ef27cda61fd486b9c
Gina <ginapenagos80@gmail.com>
2026-09-17 11:20:12 -0500

    lista_circular.py

--- lista_circular.py
+++ lista_circular.py
@@ -31,6 +31,35 @@ class listaCircular:
             nuevo.siguiente = self.primero  # el nuevo último apunta al primero
         print(f"Se ha agregado el animal: {dato.nombre} con código {dato.codigo}")
 
+    def eliminar(self, codigo):
+            if self.primero is None:
+                print("La lista está vacía")
+                return
+            actual = self.primero
+            anterior = None
+            while True:
+                if actual.dato.codigo == codigo:
+                    if anterior is None:  # Se elimina el primer nodo
+                        if actual.siguiente == self.primero:  # Solo hay un nodo
+                            self.primero = None
+                        else:
+                            # Encontrar el último nodo para actualizar su siguiente
+                            ultimo = self.primero
+                            while ultimo.siguiente != self.primero:
+                                ultimo = ultimo.siguiente
+                            self.primero = actual.siguiente
+                            ultimo.siguiente = self.primero
+                    else:
+                        anterior.siguiente = actual.siguiente
+                    print(f"Se ha eliminado el animal con código {codigo}")
+                    return
+                anterior = actual
+                actual = actual.siguiente
+                if actual == self.primero:
+                    break
+            print(f"No se encontró un animal con código {codigo}")
+    
+
     def mostrar(self):
         if self.primero is None:
             print("La lista está vacía")
@@ -42,9 +71,34 @@ class listaCircular:
             if actual == self.primero:
                 break
 
+    def alimentar_todos(self):
+        if self.primero is None:
+            print("La lista está vacía")
+            return
+        actual = self.primero
+        while True:
+            print(f"Alimentando a {actual.dato.nombre}")
+            actual = actual.siguiente
+            if actual == self.primero:
+                break
+
+    def cuidar(self):
+        if self.primero is None:
+            print("La lista está vacía")
+            return
+        actual = self.primero
+        while True:
+            print(f"revisando a {actual.dato.nombre}")
+            actual = actual.siguiente
+            if actual == self.primero:
+                break    
+
 milista = listaCircular()
 milista.append(Animal("A001", "Firulais", "Labrador", 3))
 milista.append(Animal("A002", "Michi", "Criollo", 1))
 milista.append(Animal("A003", "Rocky", "Pastor Alemán", 5))
 milista.append(Animal("A004", "Luna", "Siames", 2))
+milista.append(Animal("A005", "mougli", "serpiente", 1))
 milista.mostrar()
+milista.alimentar_todos()
+milista.cuidar()
